b0c8d6b5f73ad2304559d1235f4734a8c2f15887
dreese33 <dylanjacobreese@gatech.edu>
2018-03-24 17:07:14 -0400

    Merged Brian and Dylans efforts

--- app/src/main/java/com/example/ericreese/dual/EditProfileActivity.java
+++ app/src/main/java/com/example/ericreese/dual/EditProfileActivity.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.content.Intent;
+import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.FileNotFoundException;
 
 /**
  * Created by ericreese on 3/24/18.
@@ -19,6 +24,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private EditText bio;
     private Button back;
     private Button confirm;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,13 @@ public class EditProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Open photos, and load selected into profileImage
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
             }
         });
 
         this.name = (EditText) findViewById(R.id.name);
-
         this.bio = (EditText) findViewById(R.id.bio);
         this.back = (Button) findViewById(R.id.back);
         this.back.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +61,23 @@ public class EditProfileActivity extends AppCompatActivity {
                 //Update values to firebase
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            uri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                profileImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
