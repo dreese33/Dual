ae9014ce04b170e8f397729c38dab054642eec31
dreese33 <dylanjacobreese@gatech.edu>
2018-03-24 04:36:54 -0400

    tester

--- Desktop/Dual/app/src/main/java/com/example/ericreese/dual/MainActivity.java
+++ Desktop/Dual/app/src/main/java/com/example/ericreese/dual/MainActivity.java
@@ -7,16 +7,21 @@ import android.view.View;
 import android.content.Context;
 import android.content.Intent;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton1;
     private Button signUpButton1;
+    public static DatabaseReference mReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mReference = FirebaseDatabase.getInstance().getReference();
         this.loginButton1 = (Button) this.findViewById(R.id.loginButton);
         this.signUpButton1 = (Button) this.findViewById(R.id.signUpButton);
         this.loginButton1.setOnClickListener(new View.OnClickListener() {
